21fe627a40edcb865dba3885d0f1b9674ea24f21
ZM <782086185@qq.com>
2019-03-12 16:33:59 +0800

    多分类

---
+++ multiclass_classification.py
@@ -0,0 +1,81 @@
+from keras.datasets import reuters
+from keras import models, layers
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+(train_data, train_labels), (test_data, test_labels) = reuters.load_data(num_words=10000)
+
+
+def vectorize_sequences(sequences, dimension=10000):
+    results = np.zeros((len(sequences), dimension))
+    for i, sequence in enumerate(sequences):
+        results[i, sequence] = 1
+    return results
+
+
+x_train = vectorize_sequences(train_data)
+x_test = vectorize_sequences(test_data)
+
+
+def to_one_hot(labels, dimension=46):
+    results = np.zeros((len(labels), dimension))
+    for i, label in enumerate(labels):
+        results[i, label] = 1
+    return results
+
+
+one_hot_train_labels = to_one_hot(train_labels)
+one_hot_test_labels = to_one_hot(test_labels)
+
+
+# 模型定义
+model = models.Sequential()
+model.add(layers.Dense(64, activation='relu', input_shape=(10000,)))
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(46, activation='softmax'))
+model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+
+
+# 留出验证集
+x_val = x_train[: 1000]
+partial_x_train = x_train[1000:]
+y_val = one_hot_train_labels[: 1000]
+partial_y_train = one_hot_train_labels[1000:]
+
+
+# 训练模型
+history = model.fit(partial_x_train,
+                    partial_y_train,
+                    epochs=20,
+                    batch_size=512,
+                    validation_data=(x_val, y_val))
+
+
+def draw_loss():
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    epochs = range(1, len(loss) + 1)
+    plt.plot(epochs, loss, 'bo', label='Training loss')
+    plt.plot(epochs, val_loss, 'b', label='Validation loss')
+    plt.title('Training and Validation loss')
+    plt.xlabel('epochs')
+    plt.ylabel('loss')
+    plt.legend()
+    plt.show()
+
+
+def draw_accuracy():
+    acc = history.history['acc']
+    val_acc = history.history['val_acc']
+    epochs = range(1, len(acc) + 1)
+    plt.plot(epochs, acc, 'bo', label='Training acc')
+    plt.plot(epochs, val_acc, 'b', label='Validation acc')
+    plt.title('Training and Validation accuracy')
+    plt.xlabel('epochs')
+    plt.ylabel('accuracy')
+    plt.legend()
+    plt.show()
+
+
+draw_accuracy()
